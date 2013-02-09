a3a5ff16db7a37ed4bedb4561ee84d4f2dbdbe9f
unknown <khj63780@JD2214-04.csun.edu>
2013-02-09 13:18:50 -0800

    saving changes

--- src/info/ss12/audioalertsystem/MainActivity.java
+++ src/info/ss12/audioalertsystem/MainActivity.java
@@ -3,17 +3,26 @@ package info.ss12.audioalertsystem;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Switch;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity
+{
+	private Switch micSwitch;
+	private ButtonController buttonControl;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		buttonControl = new ButtonController();
+		micSwitch = (Switch )findViewById(R.id.mic_switch);
+		micSwitch.setOnClickListener(buttonControl);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
