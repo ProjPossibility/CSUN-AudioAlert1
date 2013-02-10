e14b126b8e5c4b24ace1d4dc833df08d0837253a
Josh L <joshua@csun>
2013-02-09 22:36:18 -0800

    Camera light - it now flickers

--- src/info/ss12/audioalertsystem/notification/CameraLightNotification.java
+++ src/info/ss12/audioalertsystem/notification/CameraLightNotification.java
@@ -2,21 +2,46 @@ package info.ss12.audioalertsystem.notification;
 
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
+import info.ss12.audioalertsystem.MainActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class CameraLightNotification extends AbstractNotification {
 
-	Camera camera;
-	Parameters pars;
+	private Camera camera;
+	private Parameters pars;
+	private Timer timer;
+	private MainActivity activity;
+	private boolean lightToggle;
 	
-	public CameraLightNotification() {
+	public CameraLightNotification(MainActivity a) {
 		camera = Camera.open();
 		pars = camera.getParameters();
+		timer = new Timer();
+		activity = a;
+		lightToggle = false;
 	}
 	
 	@Override
 	public void startNotify() {
 		pars.setFlashMode(Parameters.FLASH_MODE_TORCH);
 		camera.setParameters(pars);
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				activity.runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						if (lightToggle)
+							camera.startPreview();
+						else
+							camera.stopPreview();
+						lightToggle ^= true;
+					}
+				});
+			}
+		}, 0, 500);
 		camera.startPreview();
 	}
 
