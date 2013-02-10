124ca4ef994eee3b4ef8e7665aa7c6d4819b2a40
Jon <kyoeng.jung@gmai.com>
2013-02-10 11:00:13 -0800

    Added a sms notification class and the call for it in the main

---
+++ src/info/ss12/audioalertsystem/notification/SMSNotification.java
@@ -0,0 +1,45 @@
+package info.ss12.audioalertsystem.notification;
+
+import java.util.ArrayList;
+
+import info.ss12.audioalertsystem.MainActivity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Vibrator;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.telephony.SmsManager;
+
+
+
+public class SMSNotification extends AbstractNotification
+{
+	MainActivity main;
+	ArrayList<String> phoneNumbers;
+	
+	public SMSNotification(MainActivity main,ArrayList<String> phoneNumbers, String message) 
+	{
+		this.main = main;
+		this.phoneNumbers = phoneNumbers;
+	}
+	
+	@Override
+	public void startNotify(){
+		PendingIntent pi = PendingIntent.getActivity(main, 0,
+	            new Intent(main, MainActivity.class), 0);                
+	        SmsManager sms = SmsManager.getDefault();
+	        
+	        for (String number: phoneNumbers)
+	        {
+	        	sms.sendTextMessage(number, null, "http://goo.gl/maps/IbxHb", pi, null);  
+	        }
+	        
+	            
+	}
+	
+	@Override
+	public void stopNotify(){
+		
+	}
+
+}
