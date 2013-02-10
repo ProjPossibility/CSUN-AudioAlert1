de194ea627b01be4961f95511b4db7a6cef81214
Chris <cederstrom@acm.org>
2013-02-09 19:00:56 -0800

    Adding Notification and Alert

---
+++ src/info/ss12/audioalertsystem/notification/NotificationBarNotification.java
@@ -0,0 +1,18 @@
+package info.ss12.audioalertsystem.notification;
+
+public class NotificationBarNotification extends AbstractNotification {
+
+	public NotificationBarNotification() {
+		
+	}
+	
+	@Override
+	public void startNotify(){
+		
+	}
+	
+	@Override
+	public void stopNotify(){
+		
+	}
+}
---
+++ src/info/ss12/audioalertsystem/notification/VibrateNotification.java
@@ -0,0 +1,19 @@
+package info.ss12.audioalertsystem.notification;
+
+public class VibrateNotification extends AbstractNotification {
+	
+	public VibrateNotification(){
+		
+	}
+	
+	@Override
+	public void startNotify(){
+		
+	}
+	
+	@Override
+	public void stopNotify(){
+		
+	}
+
+}
