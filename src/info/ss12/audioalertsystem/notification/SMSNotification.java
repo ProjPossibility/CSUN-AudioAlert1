c60e2ceb05afbe50f516f036fa3b4ffbd2f95fae
igonz <no@email.com>
2013-02-10 11:47:13 -0800

    Added Listview component for Phone number list

--- src/info/ss12/audioalertsystem/notification/SMSNotification.java
+++ src/info/ss12/audioalertsystem/notification/SMSNotification.java
@@ -1,49 +1,54 @@
 package info.ss12.audioalertsystem.notification;
 
+import info.ss12.audioalertsystem.MainActivity;
+import info.ss12.audioalertsystem.alert.GPSAlert;
+
 import java.util.ArrayList;
 
-import info.ss12.audioalertsystem.MainActivity;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Vibrator;
-import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.telephony.SmsManager;
-
-
 
 public class SMSNotification extends AbstractNotification
 {
 	MainActivity main;
 	ArrayList<String> phoneNumbers;
-	
-	public SMSNotification(MainActivity main,ArrayList<String> phoneNumbers, String message) 
+
+	public SMSNotification(MainActivity main, ArrayList<String> phoneNumbers)
 	{
 		this.main = main;
 		this.phoneNumbers = phoneNumbers;
 	}
-	
+
 	@Override
-	public void startNotify(){
-		PendingIntent pi = PendingIntent.getActivity(main, 0,
-	            new Intent(main, MainActivity.class), 0);                
-	        SmsManager sms = SmsManager.getDefault();
-	        
-	        for (String number: phoneNumbers)
-	        {
-	        	sms.sendTextMessage(number, null, "http://goo.gl/maps/IbxHb", pi, null);  
-	        }
-	        
-	            
-	}
-	
-	@Override
-	public void stopNotify(){
+	public void startNotify()
+	{
+		if(GPSAlert.CUR_LATITUDE == null || GPSAlert.CUR_LONGITUDE == null)
+			return;
 		
+		PendingIntent pi = PendingIntent.getActivity(main, 0, new Intent(main,
+				MainActivity.class), 0);
+
+		SmsManager sms = SmsManager.getDefault();
+
+		for (String number : phoneNumbers)
+		{
+			sms.sendTextMessage(number, null, createMapsString(GPSAlert.CUR_LATITUDE, GPSAlert.CUR_LONGITUDE), pi,
+					null);
+		}
+
 	}
-	private String createMapsString(double latitude, double longitude) {
+
+	@Override
+	public void stopNotify()
+	{
+
+	}
+
+	private String createMapsString(double latitude, double longitude)
+	{
 		String url = "https://maps.google.com/?q=";
-		url= url + latitude +"," + longitude;
+		url = url + latitude + "," + longitude;
 		return url;
 	}
 
