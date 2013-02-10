a187b72e6c1c94b56fc6cdc2fe9a7389b1876a40
Matt Newbill <matt@bitworkssystems.com>
2013-02-09 20:24:22 -0500

    Added startNotify() method

--- src/info/ss12/audioalertsystem/notification/FlashNotification.java
+++ src/info/ss12/audioalertsystem/notification/FlashNotification.java
@@ -1,14 +1,42 @@
 package info.ss12.audioalertsystem.notification;
 
-public class FlashNotification extends AbstractNotification {
-	
-	public FlashNotification() {
+import info.ss12.audioalertsystem.*;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.graphics.Color;
+import android.util.Log;
+import android.widget.LinearLayout;
+
+public class FlashNotification extends AbstractNotification {
+	private MainActivity mainActivity;
+	private boolean swap = false;
+	public FlashNotification(MainActivity mainActivity) {
+		this.mainActivity = mainActivity;
 	}
 	
 	@Override
 	public void startNotify(){
-		
+		Log.d("In notify","Now");
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				mainActivity.runOnUiThread(new Runnable() 
+				{
+					
+					@Override
+					public void run() 
+					{
+						LinearLayout layout = (LinearLayout) mainActivity.findViewById(R.id.layout_main);
+						layout.setBackgroundColor(swap ? Color.RED : Color.WHITE);
+						swap ^= true;
+					}
+				});
+				
+			}
+		}, 0, 500);	
 	}
 	
 	@Override
